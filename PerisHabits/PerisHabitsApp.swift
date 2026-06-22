c03ca78dff1c824499fce5bc47a60feed31044b9
Nishka Pandya <nishkapandya.us@gmail.com>
2026-06-22 18:31:01 -0400

    Initial Commit

---
+++ PerisHabits/PerisHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerisHabitsApp.swift
+//  PerisHabits
+//
+//  Created by Scholar on 6/22/26.
+//
+
+import SwiftUI
+
+@main
+struct PerisHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
