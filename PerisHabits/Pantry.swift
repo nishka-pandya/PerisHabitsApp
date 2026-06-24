fa99001798679fc3827425e744c7b4d901d1a167
Nishka Pandya <nishkapandya.us@gmail.com>
2026-06-24 03:45:03 -0400

    Created a new SwiftUI file to create a digital pantry. Tried creating a horizontal stack that can scroll. Also attempted to create a class for the food items being stored.

---
+++ PerisHabits/Pantry.swift
@@ -0,0 +1,18 @@
+//
+//  Pantry.swift
+//  PerisHabits
+//
+//  Created by Scholar on 6/24/26.
+//
+
+import SwiftUI
+
+struct Pantry: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Pantry()
+}
